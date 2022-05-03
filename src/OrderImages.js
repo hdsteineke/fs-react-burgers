fe0a1b885d38f533bf88f142ba79048c7def0875
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-05-03 15:37:11 -0700

    wrote OrderImages component and default order images displaying in DOM

--- src/OrderImages.js
+++ src/OrderImages.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 export default function OrderImages({ foodId, sideId, drinkId }) {
   return (
-    <div>OrderImages</div>
+    <div className="order-images">
+      <h3>Your Order:</h3>
+      <img src={`food-${foodId}.PNG`} />
+      <img src={`side-${sideId}.PNG`} />
+      <img src={`drink-${drinkId}.PNG`} />
+    </div>
   );
 }
